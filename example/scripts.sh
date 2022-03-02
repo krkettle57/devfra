14293ac452b31a28d3961792261e73f7e69062c8
krkettle57 <krkettle57@gmail.com>
2022-03-02 21:11:53 +0900

    add example scripts

---
+++ example/scripts.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -Ceu
+
+WORKDIR=$(
+  cd $(dirname $0)
+  pwd -P
+)
+
+init() {
+  DEVFRA_FILEPATH=$1
+  if [ ! -f "${WORKDIR}/.terraform.lock.hcl" ]; then
+    terraform init
+  fi
+
+  if [ ! -f "${WORKDIR}/tfvar.json" ]; then
+    python ../devfrapy/src/main.py $DEVFRA_FILEPATH $WORKDIR
+  fi
+}
+
+plan() {
+  DEVFRA_FILEPATH=$1
+  init $DEVFRA_FILEPATH
+  terraform plan -var-file "${WORKDIR}/config/tfvar.json"
+}
+
+apply() {
+  DEVFRA_FILEPATH=$1
+  init $DEVFRA_FILEPATH
+  terraform apply -var-file "${WORKDIR}/config/tfvar.json"
+}
+
+destroy() {
+  if [ ! -f "${WORKDIR}/tfvar.json" ]; then
+    echo "tfvar.json does not exit."
+    exit 1
+  fi
+
+  terraform destroy -var-file "${WORKDIR}/config/tfvar.json"
+}
+
+"$@"
